6a909b0b2ec6a39ffb67f369e5f4df49b545c115
Steven Lind <stevenraymondlind@gmail.com>
2022-11-28 12:29:03 -0500

    ycit27-assignment

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function getCatImage() {
-    fetch("https://api.thecatapi.com/v1/images/search")
-        .then((response) => {
-            // Resolve the promise we got from calling then() to the promise that we get from calling json()
-            response.json();
-        })
-        .then((data) => {
-            console.log(data[0].url);
-            document.getElementById("catImage").src = data[0].url;
-        });
+    const image = new Image(400)
+    document.body.appendChild(image)
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data[0].url);
+    //   document.getElementById("catImage").src = data[0].url;      
+      image.src  = data[0].url;
+    });
+}
+
+function moreCats(){
+    getCatImage()
+    const image = document.createElement('img')
+    image.src  = "https://api.thecatapi.com/v1/images/search"
 }
